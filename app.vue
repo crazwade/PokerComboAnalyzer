40f679fb350444fb6749ad244c1ee189aa7ddc13
jacky <jackywu526@gmail.com>
2024-06-20 18:34:53 +0800

    feat(Eslint): install eslint

--- app.vue
+++ app.vue
@@ -2,33 +2,32 @@
 import { useUserStore } from './stores/userStore';
 
 const userStore = useUserStore();
-
 </script>
 
 <template>
-  <div>
-    <div class="text-pink-500 bg-gray-500">
-      test tailwind
-    </div>
-    <div class="my-[20px]">
-      ===
-    </div>
-    <div class="flex flex-col gap-2">
-      exp: {{ userStore.exp }}
-      <button
-        @click="userStore.incrementExp"
-        class="w-fit py-2 px-4 bg-blue-400 rounded-md hover:bg-blue-500"
-      >
-        exp++
-      </button>
-      <button
-        @click="userStore.resetExp"
-        class="w-fit py-2 px-4 bg-blue-400 rounded-md hover:bg-blue-500"
-      >
-        reset exp
-      </button>
-    </div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div>
+		<div class="text-pink-500 bg-gray-500">
+			test tailwind
+		</div>
+		<div class="my-[20px]">
+			===
+		</div>
+		<div class="flex flex-col gap-2">
+			exp: {{ userStore.exp }}
+			<button
+				class="w-fit py-2 px-4 bg-blue-400 rounded-md hover:bg-blue-500"
+				@click="userStore.incrementExp"
+			>
+				exp++
+			</button>
+			<button
+				class="w-fit py-2 px-4 bg-blue-400 rounded-md hover:bg-blue-500"
+				@click="userStore.resetExp"
+			>
+				reset exp
+			</button>
+		</div>
+		<NuxtRouteAnnouncer />
+		<NuxtWelcome />
+	</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
-  pinia: {
-    storesDirs: ['./stores/**', './custom-folder/stores/**'],
-  },
-})
+	devtools: { enabled: true },
+	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/eslint'],
+	pinia: {
+		storesDirs: ['./stores/**', './custom-folder/stores/**'],
+	},
+	eslint: {
+		config: {
+			stylistic: true,
+		},
+	},
+});
