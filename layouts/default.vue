bcc05a62ce2469779250daae7fbaa72648364072
jacky <jackywu526@gmail.com>
2024-06-21 18:26:16 +0800

    feat(layout): create default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+import Header from './header.vue';
+</script>
+
+<template>
+	<div>
+		<Header />
+		<slot />
+	</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
