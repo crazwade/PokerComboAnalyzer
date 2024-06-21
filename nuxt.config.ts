c8d61e48df3c25038bb0daad1355baff693c144d
jacky <jackywu526@gmail.com>
2024-06-21 18:25:19 +0800

    feat(nuxt.config): 設定 alias 並且引用 tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { fileURLToPath } from 'url';
+
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/eslint'],
+	modules: ['@pinia/nuxt', '@nuxt/eslint', '@nuxt/ui'],
 	pinia: {
 		storesDirs: ['./stores/**', './custom-folder/stores/**'],
 	},
+	css: ['~/assets/style/main.css'],
 	eslint: {
 		config: {
-			stylistic: true,
+			stylistic: {
+				indent: 'tab',
+				semi: true,
+			},
 		},
+	},
+	alias: {
+		'@': fileURLToPath(new URL('./', import.meta.url)),
+	},
+	tailwindcss: {
+		configPath: './tailwind.config.cjs',
 	},
 });
