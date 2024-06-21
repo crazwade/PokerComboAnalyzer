1360149e45a77508863290431ba69c0bf195fddc
jacky <jackywu526@gmail.com>
2024-06-21 18:26:52 +0800

    modify(app.vue): 調整結構 引用 layout & page

--- app.vue
+++ app.vue
@@ -1,33 +1,8 @@
 <script setup lang='ts'>
-import { useUserStore } from './stores/userStore';
-
-const userStore = useUserStore();
 </script>
 
 <template>
-	<div>
-		<div class="text-pink-500 bg-gray-500">
-			test tailwind
-		</div>
-		<div class="my-[20px]">
-			===
-		</div>
-		<div class="flex flex-col gap-2">
-			exp: {{ userStore.exp }}
-			<button
-				class="w-fit py-2 px-4 bg-blue-400 rounded-md hover:bg-blue-500"
-				@click="userStore.incrementExp"
-			>
-				exp++
-			</button>
-			<button
-				class="w-fit py-2 px-4 bg-blue-400 rounded-md hover:bg-blue-500"
-				@click="userStore.resetExp"
-			>
-				reset exp
-			</button>
-		</div>
-		<NuxtRouteAnnouncer />
-		<NuxtWelcome />
-	</div>
+	<NuxtLayout>
+		<NuxtPage />
+	</NuxtLayout>
 </template>
