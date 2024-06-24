75b41623161475ebe585206718f345cc97bbdfff
jacky <jackywu526@gmail.com>
2024-06-24 17:00:23 +0800

    feat(CurrentTime.vue): 製作 時間的顯示組件

---
+++ components/CurrentTime.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useUserStore } from '@/stores/userStore';
+
+const userStore = useUserStore();
+</script>
+
+<template>
+	<div class="text-textColor-mark text-sm">
+		{{ userStore.currentTime }}
+	</div>
+</template>
+
+<style scoped></style>
