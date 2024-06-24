882395d8eddc6762cb1477e401a0d34430017c2d
jacky <jackywu526@gmail.com>
2024-06-24 16:59:45 +0800

    feat(userStore): 在 userStore 新增時間值的設定

--- app.vue
+++ app.vue
@@ -1,4 +1,12 @@
 <script setup lang='ts'>
+import { useUserStore } from '@/stores/userStore';
+
+const userStore = useUserStore();
+
+onMounted(() => {
+	// 設定時間
+	userStore.setCurrentTime(Date.now());
+});
 </script>
 
 <template>
--- stores/userStore.ts
+++ stores/userStore.ts
@@ -1,12 +1,21 @@
+import currentTimeFormatted from '~/common/currentTimeFormatted';
+
 export const useUserStore = defineStore('user', {
-	state: () => ({
+	state: () => (<{
+		account: string;
+		name: string;
+		exp: number;
+		timer: number;
+	}>{
 		account: 'account',
 		name: 'Eduardo',
 		exp: 0,
+		timer: Date.now(),
 	}),
 	getters: {
 		userAccount: state => state.account,
 		userExp: state => state.exp,
+		currentTime: state => currentTimeFormatted(state.timer),
 	},
 	actions: {
 		incrementExp() {
@@ -14,6 +23,17 @@ export const useUserStore = defineStore('user', {
 		},
 		resetExp() {
 			this.exp = 0;
+		},
+		setCurrentTime(time: number) {
+			this.timer = time;
+			this.startTimer();
+		},
+		startTimer() {
+			const intervalId = setInterval(() => {
+				this.timer += 1000;
+			}, 1000);
+
+			return intervalId;
 		},
 	},
 });
