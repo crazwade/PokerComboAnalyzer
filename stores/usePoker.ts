99aa1a97bb362821af774d4ea24944848ac6266d
jacky <jackywu526@gmail.com>
2024-07-30 22:26:11 +0800

    feat(usePoker): store 新增 state 手牌/getters 取的手牌/actions 洗牌和清空手牌功能

--- stores/usePoker.ts
+++ stores/usePoker.ts
@@ -7,15 +7,32 @@ type DisplayPool = {
 export const usePokerStore = defineStore('poker', {
 	state: () => (<{
 		displayPool: DisplayPool[];
+		pokerhands: DisplayPool[];
 	}>{
 		displayPool: [],
+		pokerhands: [],
 	}),
 	getters: {
 		getDisplayPool: state => state.displayPool,
+		getPokerHands: state => state.pokerhands,
 	},
 	actions: {
 		setDisplayPool(newPool: DisplayPool[]) {
 			this.displayPool = newPool;
+		},
+		newPokerHands() {
+			const newHands: DisplayPool[] = [];
+			for (let i = 0; i < 15; i++) {
+				newHands.push({
+					index: Math.floor(Math.random() * 52) + 1,
+					number: Math.floor(Math.random() * 52) + 1,
+					isFlip: true,
+				});
+			}
+			this.pokerhands = newHands;
+		},
+		clearPokerHands() {
+			this.pokerhands = [];
 		},
 		filpCard(index: number) {
 			this.displayPool[index].isFlip = !this.displayPool[index].isFlip;
