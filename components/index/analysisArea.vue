f0a8d1a6ee9431db83e41b4f6c667c85e45063cf
jacky <jackywu526@gmail.com>
2024-07-31 22:03:58 +0800

    feat(analysisArea.vue): 製作手牌展示效果

--- components/index/analysisArea.vue
+++ components/index/analysisArea.vue
@@ -4,42 +4,65 @@ import { usePokerStore } from '@/stores/usePoker.js';
 
 const pokerStore = usePokerStore();
 const isFlipping = ref<boolean>(false);
+const isSpreadOut = ref<boolean>(false);
+const colddown = ref<boolean>(false);
 
-onMounted(() => {
-	console.log('analysis');
-});
+const clear = () => {
+	colddown.value = true;
+	isSpreadOut.value = false;
+	pokerStore.clearPokerHands();
+	setTimeout(() => {
+		colddown.value = false;
+	}, 1000);
+};
+
+const flop = () => {
+	pokerStore.newPokerHands();
+	isSpreadOut.value = true;
+};
 </script>
 
 <template>
-	<div class="flex flex-col w-full gap-4">
+	<div class="flex flex-col w-full gap-4 justify-center items-center">
 		<div class="flex gap-10 w-full justify-center items-center text-white">
 			<UButton
+				v-if="!isSpreadOut && !colddown"
 				:label="'發牌'"
-				@click="pokerStore.newPokerHands()"
+				:disabled="colddown"
+				@click="flop()"
 			/>
 			<UButton
 				:label="'清空'"
 				color="red"
-				@click="pokerStore.clearPokerHands()"
+				@click="clear()"
 			/>
 		</div>
-		<div class="relative flex w-full h-full">
-			<div
+		<div
+			class="flex justify-center items-center overflow-hidden cardpool border-2 pt-10 pb-5"
+			:class="[isSpreadOut ? 'w-full' : 'w-[80px]']"
+		>
+			<div class="absolute top-14 left-1/2 -translate-x-1/2">
+				手牌
+			</div>
+			<CardItem
 				v-for="(card, index) in pokerStore.pokerhands"
 				:key="index"
-				class="absolute top-0 left-[calc(50%-40px)]"
-			>
-				<CardItem
-					:is-flipping="isFlipping"
-					:index="card.index"
-					:number="card.number"
-					:is-flip="card.isFlip"
-				/>
-			</div>
+				:is-flipping="isFlipping"
+				:number="card.number"
+				:is-flip="card.isFlip"
+			/>
 		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
+@for $i from 1 through 15 {
+  .card#{$i} {
+    left: calc(50% - 40px + #{($i * 40 - 320)}px);
+  }
+}
 
+.cardpool {
+	transition: 1s;
+}
 </style>
